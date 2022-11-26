5b9093a0fe6d6a5e5e33317525ed0fa4accedb13
Pyper Bailey <pyperbaileypie@gmail.com>
2022-11-25 20:02:20 -0700

    Coroutines

---
+++ Unity/1610_Practice/1_Color_Matching/Assets/CoroutineBehavior.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class CoroutineBehavior : MonoBehaviour
+{
+    public UnityEvent repeatEvent;
+
+    public int counterNum = 3;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+
+        while (counterNum > 0)
+        {
+            yield return wfsObj;
+            repeatEvent.Invoke();
+        }
+    }
+
+}
